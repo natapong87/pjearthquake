ba168decab09726bb97f310429a47f003a06c5e3
natapong87 <natapong87@gmail.com>
2022-11-05 17:06:09 +0700

    show data

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,12 +1,13 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_settings_screens/flutter_settings_screens.dart';
+//import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 import 'package:pjearthquake/model/data_model.dart';
-import 'package:pjearthquake/screens/information_eartquake.dart';
+//import 'package:pjearthquake/screens/information_eartquake.dart';
 import 'package:pjearthquake/utility/my_constant.dart';
 import 'package:pjearthquake/utility/my_style.dart';
-import 'package:http/http.dart' as http;
+//import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -14,50 +15,28 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  
-//List productList =[];
-//   @override
-// void initState() {
-//   super.initState();
-//   productList = [];
-// }
+  DataModel dataModel = DataModel();
+  //var dataModels = <DataModel>[];
 
-  // Future getContactData() async {
-  //   // String url = '${MyConstant().domain}/pjearthquake-master/mySQL/insertData.php';
-  //   // try{
-  //   //     Response response = await Dio().get(url);
-  //   // }catch(e){
-
-  //   // }
-    // var response = await http.get(Uri.parse("${MyConstant().domain}/pjearthquake-master/mySQL/Data.php"));
-    // if (response.statusCode == 200) {
-    //   setState(() {
-    //     productList = json.decode(response.body);
-    //   });
-    //   print(productList);
-    //   return productList;
-    // }
-  //}
-
-  late DataModel dataModel;
   @override
-  void initState(){
+  void initState() {
     super.initState();
     readData();
   }
 
-   Future readData()async{
-    String url = '${MyConstant().domain}/pjearthquake-master/mySQL/getAllData.php';
+  Future<dynamic> readData() async {
+    String url =
+        '${MyConstant().domain}/pjearthquake-master/mySQL/getAllData.php'; //เชื่อมฐานข้อมูล
     await Dio().get(url).then((value) {
-      print('value = $value');
+      //print('value = $value');
       var result = json.decode(value.data);
-     for (var map in result) {
-       dataModel = DataModel.fromJson(map);
-        print('station = ${DataModel().pickID}');
-       }
-      
+      for (var map in result) {
+        DataModel dataModel = DataModel.fromJson(map); //แปลง Json to dart
+        print('station = ${dataModel.waveformIDStationCode}');
+      }
+      return dataModel;
     });
-   }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -65,10 +44,9 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text('Eartquake alert'),
       ),
-      //drawer: Drawer(),
       body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, int index) {
+        itemCount: 10, //จำนวนข้อมูลที่เอามาแสดง
+        itemBuilder: (context, index) {
           return Card(
             elevation: 5,
             margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
@@ -76,11 +54,12 @@ class _HomeScreenState extends State<HomeScreen> {
               leading: CircleAvatar(
                 radius: 30,
                 child: FittedBox(
-                  child: Text('${DataModel().waveformIDStationCode}'),
+                  //child: Text(dataModels[index].waveformIDStationCode),
+                  child: Text('${dataModel.waveformIDStationCode}'),
                 ),
               ),
-            title: Text('${DataModel().amplitudeValue}'),
-              subtitle: Text('${DataModel().sLastModified}'),
+              title: Text('${dataModel.amplitudeValue}'),
+              subtitle: Text('${dataModel.sLastModified}'),
             ),
           );
         },
